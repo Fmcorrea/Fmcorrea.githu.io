233d7254fe0e5e2065a1c6d4e7781257728fffe2
[dyad] <fmartins.correa@gmail.com>
2026-04-07 23:33:40 +0100

    Updated src/pages/Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -59,7 +59,7 @@ const LEVELS_DATA = [
     question: "Longo ou Curto?",
     pairs: [
       { word: "LONGO", targetId: "longo", image: "longo.jpeg" },
-      { word: "CURTO", targetId: "curto", image: "cabelo curto.jpg" }
+      { word: "CURTO", targetId: "curto", image: "curto.jpeg" }
     ]
   },
   {
