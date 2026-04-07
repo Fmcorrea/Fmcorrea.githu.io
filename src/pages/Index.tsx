e052a517a68921a214919c146277216737085bda
[dyad] <fmartins.correa@gmail.com>
2026-04-07 22:48:58 +0100

    Updated src/pages/Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -25,8 +25,8 @@ const LEVELS_DATA = [
     id: 2,
     question: "Nova ou Velha?",
     pairs: [
-      { word: "NOVA", targetId: "nova", image: "Branca de " },
-      { word: "VELHA", targetId: "velha", image: "https://images.unsplash.com/photo-1581579438747-1dc8d17bbce4?w=500&q=80" }
+      { word: "NOVA", targetId: "nova", image: "Branca de neve.png" },
+      { word: "VELHA", targetId: "velha", image: "Bruxa.png" }
     ]
   },
   {
